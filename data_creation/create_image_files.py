a3d64aa749597cdf92c61c11c12cb2be99cc561a
Kate <katefa46@gmail.com>
2023-02-26 15:18:32 -0500

    Create greyscale images

--- data_creation/create_image_files.py
+++ data_creation/create_image_files.py
@@ -1,6 +1,8 @@
 import os
 import random
 import shutil
+import cv2
+from config import TEST_PATH, TRAIN_PATH, VALIDATE_PATH, X, Y
 
 def move_images(src, dest, num_files):
     files = os.listdir(src)
@@ -8,5 +10,15 @@ def move_images(src, dest, num_files):
     for file_name in random.sample(files, num_files):
         shutil.move(os.path.join(src, file_name), dest)
 
-def create_black_and_white(src, dest):
-    pass
+def create_black_and_white(data_path):
+    src = "../" + data_path + Y
+    files = os.listdir(src)
+    for file_name in files:
+        img = cv2.imread(src + file_name, cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite("../" + data_path + X + file_name, img)
+        print("writing", file_name, "in", data_path)
+
+def create_X_data():
+    create_black_and_white(TRAIN_PATH)
+    create_black_and_white(TEST_PATH)
+    create_black_and_white(VALIDATE_PATH)
