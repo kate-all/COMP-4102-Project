7eeb3d01bb48678e0a8f733e86663762aa70d024
Kate <katefa46@gmail.com>
2023-03-12 21:52:41 -0400

    Plot predicted + data together in playground

--- playground.py
+++ playground.py
@@ -4,27 +4,43 @@ from model import create_model
 from config import MODEL_PATH, VALIDATE_PATH
 import matplotlib.pyplot as plt
 from tensorflow import keras
+
+# Environment/Backend stuff
 import matplotlib as mpl
 mpl.use('tkAgg')
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 def load_model():
     return tf.keras.models.load_model(MODEL_PATH)
 
 def run_prediction(model, photo, ground_truth=None):
 
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
+    fig = plt.figure(figsize=(10, 7))
+    rows, cols = 1, 3
+
+    if ground_truth is None:
+        cols = 2
+
+    fig.add_subplot(rows, cols, 1)
     img = keras.preprocessing.image.array_to_img(photo)
-    ax.imshow(img)
-
-    if ground_truth is not None:
-
-        img = keras.preprocessing.image.array_to_img(ground_truth)
-        ax.imshow(img)
+    plt.imshow(img)
+    plt.title("Original Image")
 
     output = model.predict(np.expand_dims(photo, axis=0))
 
+    fig.add_subplot(rows, cols, 2)
     img = keras.preprocessing.image.array_to_img(output[0])
-    ax.imshow(img)
+    plt.imshow(img)
+    plt.title("Predicted")
+
+    if ground_truth is not None:
+        fig.add_subplot(rows, cols, 3)
+        img = keras.preprocessing.image.array_to_img(ground_truth)
+        plt.imshow(img)
+        plt.title("Ground Truth")
+
     plt.show()
 
 if __name__ == "__main__":
